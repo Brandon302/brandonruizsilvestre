84195e6dec01156243df5938d0494b434b15fd4a
Brandon <111321835+Brandon302@users.noreply.github.com>
2023-09-21 12:13:33 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+echo "<link rel=stylesheet href=login.css>";
+$servername = "localhost";
+$username = "root";
+$password = "brandon2102";
+$dbname = "consolas";
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['password'];
+
+
+//echo "la clave es: ". $clave . "nombre es ". $nombre;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//include ('connect.php');
+
+//$cmd5 = md5($contrasena);
+$sql = "SELECT * FROM usuario WHERE usuario='$usuario' and contraseña = '$contrasena'";
+
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  //$row = $result->fetch_assoc()
+  echo '<script>
+		alert("clave correcta")
+   </script>';
+   header("Location: pagina.html");
+   exit();
+   
+
+} else {
+    echo '<script>
+    alert("clave o login incorrecto")
+    var acceso = 0
+    </script>';
+    header("Location: login.html");
+    exit();
+   
+
+}
+
+$conn->close();
+?>
